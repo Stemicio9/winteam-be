af658b3e917120503b6453e302ebb8574b184295
SimoneBr <simonesuper555@gmail.com>
2022-10-01 10:58:09 +0200

    first commit

---
+++ src/main/java/com/workonenight/winteambe/WinteamBeApplication.java
@@ -0,0 +1,13 @@
+package com.workonenight.winteambe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinteamBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinteamBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/workonenight/winteambe/WinteamBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workonenight.winteambe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinteamBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
